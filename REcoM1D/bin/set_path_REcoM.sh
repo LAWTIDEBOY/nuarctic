e6a1021d2b158c54bc91dfe9f67ee5caf0915495
fbirrien <93515489+fbirrien@users.noreply.github.com>
2022-05-05 18:36:27 +0200

    Delete set_path_REcoM.sh

--- REcoM1D/bin/set_path_REcoM.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-export RECOM_MAIN_DIR=/home/fbirrien/NuArctic/nuarctic/REcoM1D
-export RECOM_NAMELIST_PATH=${RECOM_MAIN_DIR}/config/
-export RECOM_GRID_PATH=${RECOM_MAIN_DIR}/grid/
-export RECOM_DATA_PATH=${RECOM_MAIN_DIR}/data/
-export RECOM_FORCING_PATH=${RECOM_MAIN_DIR}/forcing/
-export RECOM_RESULT_PATH=${RECOM_MAIN_DIR}/results/
-
