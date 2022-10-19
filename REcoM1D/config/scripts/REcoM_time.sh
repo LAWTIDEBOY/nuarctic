8e928b51e81ccfecb5e4f4a59bd775ec53a02e71
fbirrien <florent.birrien@awi.de>
2022-10-19 15:22:30 +0200

    add time consistency with time.recom

--- REcoM1D/config/scripts/REcoM_time.sh
+++ REcoM1D/config/scripts/REcoM_time.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -f
 
-# run python scripts that read the time configuration file 'time.recom' and create temporary file
+rm time_configuration_file.txt test.clock
+# run python scripts that read the time configuration file 'time.recom' and create temporary time file and time.clock
 ./write_time_configuration_REcoM.py
 
 # change the information in the time configuration REcoM namelist 
 ./parse_namelist.sh ../namelist.config time_configuration_file.txt
+
+# update time.clock in results directory
+rm ../../results/test.clock
+cp test.clock ../../results/.
