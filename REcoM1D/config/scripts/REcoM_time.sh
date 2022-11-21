1288c47a5073a58618e2cefa427984c66d477b51
fbirrien <florent.birrien@awi.de>
2022-11-21 14:28:55 +0100

    namelist updates

--- REcoM1D/config/scripts/REcoM_time.sh
+++ REcoM1D/config/scripts/REcoM_time.sh
@@ -2,7 +2,7 @@
 
 rm time_configuration_file.txt test.clock
 # run python scripts that read the time configuration file 'time.recom' and create temporary time file and time.clock
-./write_time_configuration_REcoM.py
+python3 write_time_configuration_REcoM.py
 
 # change the information in the time configuration REcoM namelist 
 ./parse_namelist.sh ../namelist.config time_configuration_file.txt
